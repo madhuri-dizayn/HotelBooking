226b90052da5b044488fef81980fd2b5b3b3d801
Madhuri <madhuri@dizayn.io>
2024-04-01 18:06:11 +0530

    Changes updated.

---
+++ src/app/(auth)/hotel/new/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddHotelForm from "@/components/hotel/AddHotelForm";
+
+const Hotel = () => {
+  return (
+    <div>
+      <AddHotelForm />
+    </div>
+  );
+};
+
+export default Hotel;
+
+//madhu
